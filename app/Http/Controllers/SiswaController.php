79714aed08fc30287d17171cfaa93eb81ba94a16
fadlifebr2302 <fadlifebriansyah@gmail.com>
2019-07-18 23:46:57 +0700

    Standarization response & null field validation

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -9,17 +9,50 @@ class SiswaController extends Controller
 {
     //read
     public function index() {
-        return Siswa::all();
+        $data = \App\Siswa::all();
+
+        if(count($data) > 0) {
+            $res['success'] = true;
+            $res['message'] = "Data ditemukan!";
+            $res['data'] = $data;
+            return response($res);
+        } else {
+            $res['success'] = false;
+            $res['message'] = "Data tidak ditemukan!";
+            $res['data'] = [];
+            return response($res);
+        }
     }
 
     //create
     public function create(request $request) {
         $siswa = new Siswa;
-        $siswa->nama = $request->nama;
-        $siswa->alamat = $request->alamat;
-        $siswa->save();
 
-        return "Data Berhasil Masuk";
+        if($request->nama === NULL) {
+            $res['success'] = false;
+            $res['message'] = "Nama harus di isi!";
+            return response($res);
+        } else {
+            $siswa->nama = $request->nama;
+        }
+        
+        if($request->alamat === NULL) {
+            $res['success'] = false;
+            $res['message'] = "Alamat harus di isi!";
+            return response($res);
+        } else {
+            $siswa->alamat = $request->alamat;
+        }
+
+        if($siswa->save()) {
+            $res['success'] = true;
+            $res['message'] = "Sukses menyimpan!";
+            return response($res);
+        } else {
+            $res['success'] = false;
+            $res['message'] = "Gagal menyimpan!";
+            return response($res);
+        }
     }
 
     //update
@@ -28,18 +61,58 @@ class SiswaController extends Controller
         $alamat = $request->alamat;
 
         $siswa = Siswa::find($id);
-        $siswa->nama = $nama;
-        $siswa->alamat = $alamat;
-        $siswa->save();
+        
+        if($siswa === NULL) {
+            $res['success'] = false;
+            $res['message'] = "Data siswa tidak ditemukan!";
+            return response($res);
+        } else {
+            if($nama === NULL) {
+                $res['success'] = false;
+                $res['message'] = "Nama harus di isi!";
+                return response($res);
+            } else {
+                $siswa->nama = $nama;
+            }
 
-        return "Data Berhasil di Update";
+            if($alamat === NULL) {
+                $res['success'] = false;
+                $res['message'] = "Alamat harus di isi!";
+                return response($res);
+            } else {
+                $siswa->alamat = $alamat;
+            }
+
+            if($siswa->save()) {
+                $res['success'] = true;
+                $res['message'] = "Sukses mengupdate!";
+                return response($res);
+            } else {
+                $res['success'] = false;
+                $res['message'] = "Gagal mengupdate!";
+                return response($res);
+            }
+        }
     }
 
     //delete
     public function delete($id) {
         $siswa = Siswa::find($id);
-        $siswa->delete();
 
-        return "Data Berhasil di Hapus";
+        if($siswa === NULL) {
+            $res['success'] = false;
+            $res['message'] = "Data siswa tidak ditemukan!";
+            return response($res);
+        } else {
+            if($siswa->delete()) {
+                $res['success'] = true;
+                $res['message'] = "Sukses menghapus!";
+                return response($res);
+            } else {
+                $res['success'] = false;
+                $res['message'] = "Gagal menghapus!";
+                return response($res);
+            }
+        }
     }
 }
